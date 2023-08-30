98bf3957d358567a3be392581484128e41cc2955
ViolinLee <mechleechan@outlook.com>
2023-08-30 23:16:49 +0800

    main.cpp structure

--- firmware/M5Core2-ChatGLM2/src/main.cpp
+++ firmware/M5Core2-ChatGLM2/src/main.cpp
@@ -1,11 +1,42 @@
 #include <Arduino.h>
+#include <SD.h>
+#include <SPIFFS.h>
+#include <M5Unified.h>
+#include <nvs.h>
+#include <Avatar.h>
+#include <ESP32WebServer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <deque>
+#include <ArduinoJson.h>
+
+#include "ChatGLM.h"
+#include "tts_api.h"
+
+using namespace m5avatar;
+
+#define WIFI_SSID "LeeSophia"
+#define WIFI_PASS "xxxxxx"
+#define ARDUINO_M5STACK_Core2
+
+// constants
+const int MAX_TOKEN = 10;
+const int preallocateBufferSize = 50*1024;
+// set M5Speaker virtual channel (0-7)
+static constexpr uint8_t m5spk_virtual_channel = 0;
+
+// variables
+std::deque<String> tokenHistory;
+uint8_t *preallocateBuffer;
+
+// instances
+Avatar avatar;
+ChatGLM chatglm;
+ESP32WebServer server(80);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  setupM5Box();
+  startWifi();
+  
 }
 
 void loop() {
@@ -13,6 +44,33 @@ void loop() {
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void startWifi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to WiFi");
+}
+
+void setupM5Box() {
+
+  { // global setting
+    auto cfg = M5.config();
+    cfg.external_spk = true;
+    M5.begin(cfg);
+  }
+
+  { // speaker setting
+    auto spk_cfg = M5.Speaker.config();
+    // Increasing the sample_rate will improve the sound quality instead of increasing the CPU load.
+    spk_cfg.sample_rate = 96000; // default:64000 (64kHz)  e.g. 48000 , 50000 , 80000 , 96000 , 100000 , 128000 , 144000 , 192000 , 200000
+    M5.Speaker.config(spk_cfg);
+    M5.Speaker.begin();
+  }
+
+
+{
+
+}
 }
