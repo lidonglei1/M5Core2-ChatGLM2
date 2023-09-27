db7d5c8685fcbbbd5ec52d1f8e7ee6db39669916
ViolinLee <mechleechan@outlook.com>
2023-09-27 23:49:03 +0800

    add lipsync

--- firmware/M5Core2-ChatGLM2/src/main.cpp
+++ firmware/M5Core2-ChatGLM2/src/main.cpp
@@ -20,6 +20,10 @@
 #include "AudioOutputM5Speaker.h"
 #include "app_sr.h"
 
+#include <sys/time.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 using namespace m5avatar;
 
 #define WIFI_SSID "LeeSophia"
@@ -37,13 +41,37 @@ std::deque<String> tokenHistory;
 uint8_t *preallocateBuffer;
 bool audioEndFlag;
 
+bool isReacting;
+bool isRandomBehaving;
+unsigned long lastInterfacingTime;
+
+// queue
+QueueHandle_t queueSample;
+SemaphoreHandle_t xSemaphore;
+
 // instances
 Avatar avatar;
 ChatGLM chatglm;
 AsyncWebServer server(80);
-
 static AudioOutputM5Speaker out(&M5.Speaker, m5spk_virtual_channel);
 
+// task handlers
+TaskHandle_t DisplayTaskHandle;
+TaskHandle_t SampleAudioTaskHandle;
+TaskHandle_t OnlineWakeupTaskHandle;
+TaskHandle_t SpeechInteractionTaskHandle;
+TaskHandle_t MovementTaskHandle;
+TaskHandle_t RandomBehaviorTaskHandle;
+
+// Function Declaration
+void displayTask(void * parameter);
+void sampleAudioTask(void * parameter);
+void onlineWakeupTask(void * parameter);
+void speechInteractionTask(void * parameter);
+void movementTask(void * parameter);
+void randomBehaviorTask(void * parameter);
+
+// Initialization Routine
 void setup() {
   setupM5Box();
   startWifi();
@@ -51,8 +79,19 @@ void setup() {
   ESP_LOGI(TAG, "speech recognition start");
   app_sr_start(false);
   
+  // create task
+  xTaskCreatePinnedToCore(displayTask, "DisplayTask",  10000,  NULL,  1, &DisplayTaskHandle, 1);
+  xTaskCreatePinnedToCore(sampleAudioTask, "SampleAudioTask",  10000,  NULL,  1, &SampleAudioTaskHandle, 1);
+  xTaskCreatePinnedToCore(onlineWakeupTask, "OnlineWakeupTask",  4000,  NULL,  1, &OnlineWakeupTaskHandle, 1);
+  xTaskCreatePinnedToCore(speechInteractionTask, "SpeechInteractionTask",  4000,  NULL,  1, &SpeechInteractionTaskHandle, 1);
+  xTaskCreatePinnedToCore(movementTask, "MovementTask",  4000,  NULL,  1, &MovementTaskHandle, 1);
+  xTaskCreatePinnedToCore(randomBehaviorTask, "RandomBehaviorTask",  4000,  NULL,  1, &RandomBehaviorTaskHandle, 1);
+  avatar.addTask(c, "lipSync");
+
+  Serial.println("Finish Setuped.");
 }
 
+// Main Program
 void loop() {
   // 
 
@@ -91,4 +130,71 @@ static void audio_play_finish_cb(void)
     if (audioEndFlag) {
         audioEndFlag = true;
     }
+}
+
+void displayTask(void * parameter) {
+  while (true) {
+    
+  }
+  vTaskDelete(DisplayTaskHandle);
+}
+
+void sampleAudioTask(void * parameter) {
+  while (true) {
+    
+  }
+  vTaskDelete(SampleAudioTaskHandle);
+}
+
+void onlineWakeupTask(void * parameter) {
+  while (true) {
+
+  }
+  vTaskDelete(OnlineWakeupTaskHandle);
+}
+
+void speechInteractionTask(void * parameter) {
+  while (true) {
+
+  }
+  vTaskDelete(SpeechInteractionTaskHandle);
+}
+
+void movementTask(void * parameter) {
+  while (true) {
+
+  }
+  vTaskDelete(MovementTaskHandle);
+}
+
+void randomBehaviorTask(void * parameter) {
+  while (true) {
+
+  }
+  vTaskDelete(RandomBehaviorTaskHandle);
+}
+
+/**
+ * https://github.com/robo8080/M5Unified_StackChan_ChatGPT
+*/
+void lipSync(void *args)
+{
+  float gazeX, gazeY;
+  int level = 0;
+  DriveContext *ctx = (DriveContext *)args;
+  Avatar *avatar = ctx->getAvatar();
+  for (;;)
+  {
+    level = abs(*out.getBuffer());
+    if(level<100) level = 0;
+    if(level > 15000)
+    {
+      level = 15000;
+    }
+    float open = (float)level/15000.0;
+    avatar->setMouthOpenRatio(open);
+    avatar->getGaze(&gazeY, &gazeX);
+    avatar->setRotation(gazeX * 5);
+    delay(50);
+  }
 }
